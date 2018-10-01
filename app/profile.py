d9264abb18bfe5c3296aff1c9a7c66a60dab659f
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-10-01 16:20:30 -0700

    Added basic auth and profile

---
+++ app/profile.py
@@ -0,0 +1,24 @@
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for
+)
+from werkzeug.exceptions import abort
+from app.auth import login_required
+from app.db import get_db
+
+bp = Blueprint('profile', __name__, url_prefix='/profile')
+
+
+@bp.route('/<int:user_id>', methods=('GET', 'POST'))
+def index(user_id):
+    db = get_db()
+    info = db.execute(
+        'SELECT * FROM user WHERE id = ?',
+        (user_id,)
+    )
+    return render_template('profile/index.html', info=list(info.fetchone()))
