b4289cb29827e6ce4f2fa36f70ca6576ce962fd3
Christopher Sumnicht <csumnicht@berkeley.edu>
2018-10-15 16:28:56 -0700

    Added point system into database, plus basic profile stuff to demonstrate the point system works

--- app/schema.sql
+++ app/schema.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS history;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXt NOT NULL
+  password TEXT NOT NULL,
+  points   INTEGER DEFAULT 0
+);
+
+CREATE TABLE history (
+  id INTEGER,
+  award INTEGER,
+  time_stamp INTEGER
 );
