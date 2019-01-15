dd94bcee691c9d8a4fb51931537ae7612f5c393c
Christopher Sumnicht <csumnicht@berkeley.edu>
2019-01-15 11:19:55 -0800

    Removed some unecessary html files, further work needs to be done on displaying them

--- app/problems.py
+++ app/problems.py
@@ -44,32 +44,32 @@ render_problem = functools.partial(render_template, categories=categories)
 
 @bp.route('/alignment.html')
 def alignment():
-    return render_problem('problems/alignment.html')
+    return render_problem('problems/default.html')
 
 
 @bp.route('/protein.html')
 def protein():
-    return render_problem('problems/protein.html')
+    return render_problem('problems/default.html')
 
 
 @bp.route('/motifs.html')
 def motifs():
-    return render_problem('problems/motifs.html')
+    return render_problem('problems/default.html')
 
 
 @bp.route('/rna.html')
 def rna():
-    return render_problem('problems/rna.html')
+    return render_problem('problems/default.html')
 
 
 @bp.route('/phylogeny.html')
 def phylogeny():
-    return render_problem('problems/phylogeny.html')
+    return render_problem('problems/default.html')
 
 
 @bp.route('/probability.html')
 def probability():
-    return render_problem('problems/probability.html')
+    return render_problem('problems/default.html')
 
 
 @bp.route('test.html', methods=('GET',))
