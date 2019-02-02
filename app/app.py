85a791aec051f2241d674dca84ec3db47f494849
Christopher Sumnicht <csumnicht@berkeley.edu>
2019-02-01 20:02:47 -0800

    Move CSRF to inside create app instead of outside. Removed a global as a result

--- app/app.py
+++ app/app.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
 
 
-csrf = CSRFProtect()
-
-
 def create_app(test_config=None):
     # TODO Avoid global
     app = Flask(__name__, instance_relative_config=True)
+    csrf = CSRFProtect()
     # TODO Secret key?
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
     app.secret_key = 'dev'
