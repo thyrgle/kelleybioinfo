c76e5816788011df5394cd3571d0027979422204
Christopher Sumnicht <csumnicht@berkeley.edu>
2019-02-07 17:26:18 -0800

    Fixed bug with level select so that cells can be selected after level change

--- app/static/matrix.js
+++ app/static/matrix.js
@@ -2,13 +2,16 @@
  * Deals with selection of a cell in a matrix. In particular, toggles the cell
  * between yellow and clear.
  */
-$( ".cell" ).click(function() {
+$(function() {
+  $(document.body).on('click', '.cell', function() {
+    console.log("HERE");
     // TODO Refactor the if statement?
     $( this ).toggleClass( "selected-cell" );
     var answerInput = $( this ).find("input[value|='selected']");
     if (answerInput.val() === 'selected') {
-        answerInput.val('selected-not');
+      answerInput.val('selected-not');
     } else {
-        answerInput.val('selected');
+      answerInput.val('selected');
     }
+  });
 });
