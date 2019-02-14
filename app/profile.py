19eb91da3d17e81381130a836356bb7add6db144
Christopher Sumnicht <csumnicht@berkeley.edu>
2019-02-13 16:08:28 -0800

    Simplified category awarding code

--- app/profile.py
+++ app/profile.py
@@ -10,33 +10,25 @@ from libgravatar import Gravatar
 bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 
+def get_category_points(user_id, category):
+    return models.History.query.with_entities(
+        func.sum(models.History.value)
+    ).filter_by(user_id=user_id, category=category).first()[0]
+
+
 @bp.route('/<int:user_id>', methods=('GET', 'POST'))
 def index(user_id):
     # TODO Is [0] the best option?
-    # TODO Refactor redundancy!
+    # TODO Simplify further using kwargs?
     total_rep = models.History.query.with_entities(
             func.sum(models.History.value)
         ).filter_by(user_id=user_id).first()[0]
-    print(total_rep)
-    alignment_rep = models.History.query.with_entities(
-            func.sum(models.History.value)
-        ).filter_by(user_id=user_id, category="Alignment").first()[0]
-    print(alignment_rep)
-    protein_rep = models.History.query.with_entities(
-            func.sum(models.History.value)
-        ).filter_by(user_id=user_id, category="Protein").first()[0]
-    motifs_rep = models.History.query.with_entities(
-            func.sum(models.History.value)
-        ).filter_by(user_id=user_id, category="Motifs").first()[0]
-    rna_rep = models.History.query.with_entities(
-            func.sum(models.History.value)
-        ).filter_by(user_id=user_id, category="RNA").first()[0]
-    phylogeny_rep = models.History.query.with_entities(
-            func.sum(models.History.value)
-        ).filter_by(user_id=user_id, category="Phylogeny").first()[0]
-    probability_rep = models.History.query.with_entities(
-            func.sum(models.History.value)
-        ).filter_by(user_id=user_id, category="Probability").first()[0]
+    alignment_rep = get_category_points(user_id, "Alignment")
+    protein_rep = get_category_points(user_id, "Protein")
+    motifs_rep = get_category_points(user_id, "Motifs")
+    rna_rep = get_category_points(user_id, "RNA")
+    phylogeny_rep = get_category_points(user_id, "Phylogeny")
+    probability_rep = get_category_points(user_id, "Probability")
     name = models.User.query.filter_by(user_id=user_id).first()
     img = Gravatar(name.email).get_image(default='identicon', size=200)
     return render_template('profile/index.html',
